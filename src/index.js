72a920591e154a9fa2b189b82032e44bd5f1e53f
eastflag <dklee98@>
2021-01-07 18:36:48 +0900

    redux middleware

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {applyMiddleware, createStore} from "redux";
+import rootReducer from "./modules";
+import {Provider} from "react-redux";
+import myLogger from "./middlewares/myLogger";
+import ReduxThunk from 'redux-thunk';
+import {logger} from "redux-logger/src";
+import {BrowserRouter} from "react-router-dom";
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk, logger));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
