5678dd729a5c62a8b34f4b2cf2b31e240171c4c0
eastflag <dklee98@>
2021-02-09 14:16:14 +0900

    redux-saga 적용

--- src/index.js
+++ src/index.js
@@ -4,15 +4,33 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {applyMiddleware, createStore} from "redux";
-import rootReducer from "./modules";
+import rootReducer, {rootSaga} from "./modules";
 import {Provider} from "react-redux";
 import myLogger from "./middlewares/myLogger";
 import ReduxThunk from 'redux-thunk';
 import {logger} from "redux-logger/src";
 import {BrowserRouter} from "react-router-dom";
 import {composeWithDevTools} from "redux-devtools-extension";
+import createSagaMiddleware from 'redux-saga';
+import {createBrowserHistory} from "history";
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(ReduxThunk, logger)));
+const customHistory = createBrowserHistory();
+const sagaMiddleware = createSagaMiddleware(); // 사가 미들웨어를 만듭니다.
+
+const store = createStore(
+  rootReducer,
+  // logger 를 사용하는 경우, logger가 가장 마지막에 와야합니다.
+  composeWithDevTools(
+    applyMiddleware(
+      ReduxThunk.withExtraArgument({ history: customHistory }),
+      sagaMiddleware, // 사가 미들웨어를 적용하고
+      logger
+    )
+  )
+); // 여러개의 미들웨어를 적용 할 수 있습니다.
+
+sagaMiddleware.run(rootSaga); // 루트 사가를 실행해줍니다.
+// 주의: 스토어 생성이 된 다음에 위 코드를 실행해야합니다.
 
 ReactDOM.render(
   <BrowserRouter>
